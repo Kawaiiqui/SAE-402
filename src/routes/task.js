d989088f3bf2e9b9dfeec81645d8c77e3e222a95
Kawaiiqui <christellegineston@gmail.com>
2025-03-31 10:18:45 +0200

    config

--- src/routes/task.js
+++ src/routes/task.js
@@ -4,4 +4,35 @@ const router = express.Router();
 router.get( "/", async(req, res) => {
 res.send ("No task defined.");
 });
+
+router.post("/", async(req, res) => {
+    if(req.body){
+    res.json({message: "body is defined.", data: req.body});
+    }else{
+        res
+            .status(500)
+            .json({message: "You forgot the body part in your request !"});
+    }
+});
+
+router.post("/", async(req, res) => {
+    if(req.body) {
+    res.json({message: "body is defined." , data: req.body});
+    }else{
+    res
+    .status(500)
+    .json({message: "You forgot the body part in your request!"});
+    }
+});
+
+router.delete("/:id", async(req, res) => {
+    if(req.params.id){
+    res.json({message: 'Task ${req.params.id} has been removed.'});
+    }else{
+    res
+    .status(500)
+    .json({message: "You forgot the id param in your request"});
+    }
+    });
+
 module.exports = router;
