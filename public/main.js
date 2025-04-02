5ca7b16d780f36aebf2ea0421b65367bc988867f
Kawaiiqui <christellegineston@gmail.com>
2025-04-02 15:31:41 +0200

    tout s'affiche :3

--- public/main.js
+++ public/main.js
@@ -1,12 +1,9 @@
-const height = Math.min(500, width / 2);
-const outerRadius = height / 2 - 10;
-const innerRadius = outerRadius * 0.75;
 var chart = document.getElementById("pie-chart");
 var game = document.getElementById("game");
 
 function startGame() {
-    
-}// https://tauday.com/tau-manifesto
+        
+}
 
 
 game.addEventListener("click", function () { return startGame("classic"); });
