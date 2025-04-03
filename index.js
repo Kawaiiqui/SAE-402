f5c7561b5c28fbc4434c4795d6bc30d2f0dd08c7
Kawaiiqui <christellegineston@gmail.com>
2025-04-03 13:47:50 +0200

    U N (1) point :3

--- index.js
+++ index.js
@@ -16,40 +16,12 @@ optionsSuccessStatus: 200
 };
 
 app.use(express.json()).use(cors(corsOptions));
-app.use( "/movies", movieRoute);
+app.use("/movies", movieRoute);
 app.use( "/actors", actorRoute);
 app.use( "/genre", genreRoute);
 app.use( "/moviesactors", moviesactorRoute);
 app.use( "/moviesgenre", moviesgenreRoute);
 app.use(express.json());
-/*
-app.get('/movies', async (req, res) => {
-  try {
-    // RQT SQL
-    const data = await myDB.query(`
-      SELECT *
-      FROM movies
-      JOIN moviesgenre ON movies.id = moviesgenre.id_movie
-      JOIN genres ON genres.id = moviesgenre.id_genre
-      JOIN moviesactors ON moviesactors.id_movie = movies.id
-      JOIN actors ON actors.id = moviesactors.id_actor
-      ORDER BY movies.year
-    `, { type: myDB.QueryTypes.SELECT });
-    
-    // Transformer les données dans le format attendu par ton graphique
-    const formattedData = data.map(item => [
-      item.year,                  // Position x (année)
-      Math.random() * 100,        // Position y (valeur à déterminer selon ton cas)
-      item.genre                  // Catégorie pour la couleur
-    ]);
-    
-    res.json(formattedData);
-  } catch (error) {
-    console.error('Erreur lors de la récupération des données :', error);
-    res.status(500).json({ message: 'Erreur serveur' });
-  }
-});
-*/
 app.get("/", (req,res)=> {
     const fullPath = path.join(__dirname, 'public', 'index.html');
     res.sendFile(fullPath);
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -12,7 +12,7 @@ router.get( "/", async(req, res) => {
     });
     } catch(error){
         res.status(500).json({
-            message: "Failed to fetch movies. xxx", error
+            message: "Failed to fetch movies. xxx"
         });
     }
 });
